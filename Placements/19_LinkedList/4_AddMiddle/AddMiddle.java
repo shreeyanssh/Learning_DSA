4441b666491673be3ce3037e660c4d62fb792e1f
Shreyansh Tyagi <shtyagi07@gmail.com>
2024-02-02 15:27:20 +0530

    Add Middle

---
+++ Placements/19_LinkedList/4_AddMiddle/AddMiddle.java
@@ -0,0 +1,58 @@
+public class AddMiddle {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }    
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void addMiddle(int data){
+
+    }
+
+    public void printLL(Node node){
+        if(node == null){
+            System.out.print("Null");
+            return;
+        }
+        System.out.print(node.data + "->");
+        printLL(node.next);
+    }
+
+    public static void main(String[] args){
+        AddMiddle list = new AddMiddle();
+
+        list.addFirst(2);
+        list.addFirst(1);
+        list.addLast(3);
+        list.addLast(4);
+
+        list.printLL(list.head);
+    }
+}
